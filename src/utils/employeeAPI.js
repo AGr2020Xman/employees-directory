ee9741fc1b2567fd4a218796af35962e085c7fe1
Andre Grech <silver.grech@gmail.com>
2021-01-15 16:12:36 +1100

    return axios call

--- src/utils/employeeAPI.js
+++ src/utils/employeeAPI.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default {
   getEmployees: function () {
-    axios.get(
+    return axios.get(
       "https://randomuser.me/api/?results=50&inc=name,phone,picture,email,dob&nat=au"
     );
   },
