4003cdd9a541640f5e81a956f3c0114dec91f161
AGr2020Xman <silver.grech@gmail.com>
2021-01-16 15:57:13 +1100

    reverted 1 commit

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from "react";
 import TableHeader from "./TableHeader";
 import TableData from "./TableData";
 
-export const Table = ({ normalizeData,  }) => {
+export const Table = ({ normalizeData }) => {
+  const [headerMeta, setHeaderMeta] = useState(meta);
+  const [tableData, setTableData] = useState([]);
 
   useEffect(() => {
     // normalize data
-    setEmployees(normalizeData(employees), meta);
+    setTableData(normalizeData(data), meta);
   }, []);
 
   return (
