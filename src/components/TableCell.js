2feda77e82284a62fb5d68481efd8f8ecf694d16
AGr2020Xman <silver.grech@gmail.com>
2021-01-18 16:33:59 +1100

    updates to file structure. additional add to gitignore

--- src/components/TableCell.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function TableCell({ data }) {
-  return (
-    <td className="table-cell" onClick={data.sortFunc}>
-      {data.text}
-    </td>
-  );
-};
